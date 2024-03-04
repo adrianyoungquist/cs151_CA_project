9ea7bf44c062760e7d8f036d90fba3f5b20dcb3e
adrianyoungquist <adrian.youngquist@gmail.com>
2024-03-04 13:08:11 -0800

    My MVC framework and stopLight

---
+++ v0/mvcApps/src/main/java/mvc/View.java
@@ -0,0 +1,27 @@
+package mvc;
+
+
+import javax.swing.*;
+
+public class View extends JPanel implements Subscriber {
+    protected Model model;
+
+    public View() {
+        // use setModel to init model
+    }
+
+    public View(Model model) {
+        this.model = model;
+        model.subscribe(this);
+    }
+
+    public void setModel(Model model) {
+        this.model = model;
+        model.subscribe(this);
+    }
+
+    @Override
+    public void update() {
+        repaint();
+    }
+}
