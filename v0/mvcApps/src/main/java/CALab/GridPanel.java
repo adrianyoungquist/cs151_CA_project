273b387f2d70dd5b78215fdc7e5fcd065aae778f
= <adrian.youngquist@gmail.com>
2024-03-14 10:07:47 -0700

    Fix layout of buttons on GridPanel

--- v0/mvcApps/src/main/java/CALab/GridPanel.java
+++ v0/mvcApps/src/main/java/CALab/GridPanel.java
@@ -4,38 +4,61 @@ import mvc.AppFactory;
 import mvc.AppPanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class GridPanel extends AppPanel {
     public GridPanel(AppFactory factory) {
         super(factory);
+        controlPanel.setLayout(new BorderLayout());
         controlPanel.add(new ControlPanel());
     }
 
-    // TODO ???
     class ControlPanel extends JPanel {
         public ControlPanel() {
-            //setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+            JPanel rowOnePanel = new JPanel();
+            rowOnePanel.setLayout(new BoxLayout(rowOnePanel, BoxLayout.X_AXIS));
+
+            rowOnePanel.add(Box.createHorizontalGlue());
+
             JButton run1 = new JButton("RUN1");
             run1.addActionListener(GridPanel.this);
-            add(run1);
-            //run1.setAlignmentX(Component.LEFT_ALIGNMENT);
+            rowOnePanel.add(run1);
+
+            rowOnePanel.add(Box.createHorizontalGlue());
 
             JButton run50 = new JButton("RUN50");
             run50.addActionListener(GridPanel.this);
-            add(run50);
-            //run50.setAlignmentX(Component.RIGHT_ALIGNMENT);
+            rowOnePanel.add(run50);
+
+            rowOnePanel.add(Box.createHorizontalGlue());
+
+            add(rowOnePanel);
+            rowOnePanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
             add(Box.createVerticalGlue());
 
+
+            JPanel rowTwoPanel = new JPanel();
+            rowTwoPanel.setLayout(new BoxLayout(rowTwoPanel, BoxLayout.X_AXIS));
+
+            rowTwoPanel.add(Box.createHorizontalGlue());
+
             JButton populate = new JButton("POPULATE");
             populate.addActionListener(GridPanel.this);
-            add(populate);
-            //populate.setAlignmentX(Component.LEFT_ALIGNMENT);
+            rowTwoPanel.add(populate);
+
+            rowTwoPanel.add(Box.createHorizontalGlue());
 
             JButton clear = new JButton("CLEAR");
             clear.addActionListener(GridPanel.this);
-            add(clear);
-            //populate.setAlignmentX(Component.RIGHT_ALIGNMENT);
+            rowTwoPanel.add(clear);
+
+            rowTwoPanel.add(Box.createHorizontalGlue());
+
+            add(rowTwoPanel);
+            rowTwoPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
 
             add(Box.createVerticalGlue());
         }
